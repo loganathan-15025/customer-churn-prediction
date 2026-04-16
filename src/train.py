97641feb55f5235b6323bf4f2a64104539c23d55
loganathan-15025 <thisislogan15@gmail.com>
2026-04-16 17:19:09 +0530

    Split dataset into features and target, and create training and testing sets

--- src/train.py
+++ src/train.py
@@ -1,4 +1,6 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
+
 
 df = pd.read_csv("../data/churn.csv")
 print("Dataset Loaded Successfully ✅")
@@ -20,3 +22,17 @@ df = pd.get_dummies(df, drop_first=True)
 print("\nDataset after encoding:")
 print(df.head())
 print("\nNew shape after encoding:", df.shape)
+
+#train test split
+
+X = df.drop("Churn_Yes", axis=1)
+y = df["Churn_Yes"]
+
+print("\nFeature shape:", X.shape)
+print("Target shape:", y.shape)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+print("\nTraining set:", X_train.shape)
+print("Testing set:", X_test.shape)
