379d7115fd3c96d4d27dd5321d7810ae0199eabf
loganathan-15025 <thisislogan15@gmail.com>
2026-04-16 17:11:21 +0530

    dataset cleaning process convert TotalCharges to numeric, handle missing values, and remove customerID column

--- src/train.py
+++ src/train.py
@@ -4,3 +4,13 @@ df = pd.read_csv("../data/churn.csv")
 print("Dataset Loaded Successfully ✅")
 print("Shape:", df.shape)
 print(df.head())
+
+
+#data cleaning
+
+df['TotalCharges'] = pd.to_numeric(df['TotalCharges'], errors='coerce')
+print("\nMissing values before cleaning:\n", df.isnull().sum())
+df.dropna(inplace=True)
+df.drop("customerID", axis=1, inplace=True)
+print("\nMissing values after cleaning:\n", df.isnull().sum())
+print("\nDataset shape after cleaning:", df.shape)
