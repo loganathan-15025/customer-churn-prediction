d42658df488320105e010e91af49736dcb7dbe8e
loganathan-15025 <thisislogan15@gmail.com>
2026-04-16 17:15:43 +0530

    Encode categorical variables using one-hot encoding

--- src/train.py
+++ src/train.py
@@ -14,3 +14,9 @@ df.dropna(inplace=True)
 df.drop("customerID", axis=1, inplace=True)
 print("\nMissing values after cleaning:\n", df.isnull().sum())
 print("\nDataset shape after cleaning:", df.shape)
+
+#feature engineering
+df = pd.get_dummies(df, drop_first=True)
+print("\nDataset after encoding:")
+print(df.head())
+print("\nNew shape after encoding:", df.shape)
